4ce759f1716429d2d1006c22e57b3a8c1c8da94d
Serkan Sipahi <serkan.sipahi@yahoo.de>
2014-04-05 22:41:34 +0200

    - tests/refactoring/improvements

--- taby.js
+++ taby.js
@@ -12,6 +12,12 @@ var Taby = (function(document, window, undefined){
     Array.prototype.$each = function(callback){
 
     };
+    Object.prototype.$each = function(callback){
+
+    };
+    String.prototype.$each = function(callback){
+
+    };
     Element.prototype.$addClass = function(classname){
 
         if(_feature.classList){
@@ -56,6 +62,11 @@ var Taby = (function(document, window, undefined){
 
     };
     Element.prototype.$closest = function(expression){
+
+        /*
+         * @expression string
+         * dokumentieren....
+         */
 
         var $this         = this,
             pattern       = /^(\*)?(.*?)(:until\((.*?)\))?$/,
@@ -106,246 +117,246 @@ var Taby = (function(document, window, undefined){
     // >>> bind querySelectorAll to $
     var $ = document.querySelectorAll.bind(document);
 
-	// > context
-	var thisTaby = {};
+    // > context
+    var thisTaby = {};
 
-	// > Constructor
-	function Taby(){
+    // > Constructor
+    function Taby(){
 
         thisTaby = this;
 
-		// > core elements
-		this.tabNamespaces    = '.taby';
-		this.tab              = '.taby[data-tab]';
-		this.tabContent       = '.taby[data-tab-content]';
-		this.tabLockEl        = '.taby-lock',
-		this.$tabyFixedEl     = {};
-		this.$tabs            = {};
-		this.tabsLength       = 0;
-		this.tabWith          = 0;
-		this.absoluteWith     = 95;
+        // > core elements
+        this.tabNamespaces    = '.taby';
+        this.tab              = '.taby[data-tab]';
+        this.tabContent       = '.taby[data-tab-content]';
+        this.tabLockEl        = '.taby-lock',
+        this.$tabyFixedEl     = {};
+        this.$tabs            = {};
+        this.tabsLength       = 0;
+        this.tabWith          = 0;
+        this.absoluteWith     = 95;
 
-		this.$deeperTabs      = {};
-		this.deeperTabsLength = 0;
-		this.$children        = {};
-		this.childrenLength   = 0;
+        this.$deeperTabs      = {};
+        this.deeperTabsLength = 0;
+        this.$children        = {};
+        this.childrenLength   = 0;
 
-		this.lastActiveTabs   = null;
+        this.lastActiveTabs   = null;
 
-		// > init taby
-		this.initTaby();
-	}
+        // > init taby
+        this.initTaby();
+    }
 
-	// > public methods
-	Taby.prototype = {
-		initTaby : function(){
-			this.calculateTabSizes();
-			this.setEvents();
-		},
-		calculateTabSizes : function(){
+    // > public methods
+    Taby.prototype = {
+        initTaby : function(){
+            this.calculateTabSizes();
+            this.setEvents();
+        },
+        calculateTabSizes : function(){
 
-			var i = 0, d = 0, $children = {}, $childrenLength;
-			this.$tabyFixedEl = $(this.tab+' > ul > li[data-taby-fixed-size]');
-			this.$tabs        = $(this.tab+' > ul > li:not([data-taby-fixed-size])');
-			this.tabsLength   = this.$tabs.length;
-			this.tabWidth     = (this.$tabyFixedEl.length === 1 ? this.absoluteWith : 100) / this.tabsLength;
+            var i = 0, d = 0, $children = {}, $childrenLength;
+            this.$tabyFixedEl = $(this.tab+' > ul > li[data-taby-fixed-size]');
+            this.$tabs        = $(this.tab+' > ul > li:not([data-taby-fixed-size])');
+            this.tabsLength   = this.$tabs.length;
+            this.tabWidth     = (this.$tabyFixedEl.length === 1 ? this.absoluteWith : 100) / this.tabsLength;
 
-			//first level
-			for(i=0;i<this.tabsLength;i++){
-				this.$tabs[i].style.width = this.tabWidth+'%';
-			}
+            //first level
+            for(i=0;i<this.tabsLength;i++){
+                this.$tabs[i].style.width = this.tabWidth+'%';
+            }
 
-			//deeper levels
-			this.$deeperTabs      = $(this.tab+' > ul ul');
-			this.deeperTabsLength = this.$deeperTabs.length;
+            //deeper levels
+            this.$deeperTabs      = $(this.tab+' > ul ul');
+            this.deeperTabsLength = this.$deeperTabs.length;
 
 
-			for(i=0;i<this.deeperTabsLength; i++){
-				this.$children      = this.$deeperTabs[i].children;
-				this.childrenLength = this.$children.length;
-				this.tabWidth       = 100 / this.childrenLength;
-				for(d=0;d<this.childrenLength;d++){
-					this.$children[d].style.width = this.tabWidth+'%';
-				}
+            for(i=0;i<this.deeperTabsLength; i++){
+                this.$children      = this.$deeperTabs[i].children;
+                this.childrenLength = this.$children.length;
+                this.tabWidth       = 100 / this.childrenLength;
+                for(d=0;d<this.childrenLength;d++){
+                    this.$children[d].style.width = this.tabWidth+'%';
+                }
 
-			}
-		},
-		handleActiveTabs : function($target, parentElement, addClass, upToElement){
+            }
+        },
+        handleActiveTabs : function($target, parentElement, addClass, upToElement){
 
-			var i, length;
+            var i, length;
 
-			if(this.lastActiveTabs!== null){
-				for(i=0, length=this.lastActiveTabs.length;i<length;i++){
-					this.lastActiveTabs[i].$removeClass('taby-active');
-				}
-			}
-			// >>> $target.getAllClosest('li').until('.taby');
-			// >>> $target.closest('*li:until(.taby)');
+            if(this.lastActiveTabs!== null){
+                for(i=0, length=this.lastActiveTabs.length;i<length;i++){
+                    this.lastActiveTabs[i].$removeClass('taby-active');
+                }
+            }
+            // >>> $target.getAllClosest('li').until('.taby');
+            // >>> $target.closest('*li:until(.taby)');
             $target.$closest('*li:until(.taby)');
-			this.lastActiveTabs = $target.$getAllClosestClass('li', 'taby');
+            this.lastActiveTabs = $target.$getAllClosestClass('li', 'taby');
 
-			for(i=0, length=this.lastActiveTabs.length;i<length;i++){
-				this.lastActiveTabs[i].$addClass('taby-active');
-			}
-
-
-		},
-		setEvents : function(){
-
-			var event = {};
-				event.preventDefault=function(){};
-
-			var $self = this,
-				$lastShowedTab=null,
-				callback = function(e){
-
-					e.preventDefault();
-
-					// >>> *****************************
-					// >>> read operation
-
-					// >>> start event delegation auslagern
-					var target = e.target,
-						$thisAElement = {},
-						tmpUlContainer = [], i= 0, length=0;
-
-					if(e.target.nodeName.toLowerCase() === 'a'){
-						$thisAElement = e.target;
-						target = e.target.parentNode;
-					} else {
-						$thisAElement = e.target.children[0];
-						if(e.target.children.length===0){
-							return;
-						}
-					}
-
-					// >>> *******************************
-					// >>> set active class
-
-					$self.handleActiveTabs(target);
-
-					// >>> ende delegation auslagern
-
-					var $thisChildTarget = target.querySelector('ul'),
-						$allTargets      = $(thisTaby.tab + ' ul');
-
-					// > wenn kein ul, dann akuellen li auf
-					//   $thisChildTarget setzen, damit
-					//   aus dieser das parent ul ermittelt werden kann
-					if($thisChildTarget===null) {
-						$thisChildTarget = target;
-					} else {
-						tmpUlContainer.push($thisChildTarget);
-					}
-
-					// >>> start closest auslagern
-					var parentClassName='';
-					while(!/^taby$/.test(parentClassName)){
-						var $parentNode = $thisChildTarget.parentNode;
-						if($parentNode.nodeName.toLowerCase()==='ul'){
-							tmpUlContainer.push($parentNode);
-						}
-						$thisChildTarget = $parentNode;
-						parentClassName = $parentNode.className.replace(' ', '');
-
-					}
-					// >>> end closest auslagern
-
-					// >>> *****************************
-					// >>> write operation
-
-					// > alle ul´s ausblenden
-					for(i= 0, length=$allTargets.length; i<length;i++){
-						$allTargets[i].$removeClass('show');
-					}
-
-					// > alle ul´s anzeigen die angezeigt werden sollen
-					for(i=0, length=tmpUlContainer.length;i<length;i++){
-						tmpUlContainer[i].$addClass('show');
-					}
-
-					// >>> *****************************
-					// >>> set height of tab wrapper
-
-					var maxHeightOfTab    = 0,
-						$visibleListLayer = $($self.tab + ' ul.show > li:first-child');
-
-					for(i=0, length=$visibleListLayer.length;i<length;i++){
-						maxHeightOfTab += $visibleListLayer[i].clientHeight;
-
-					}
-					$($self.tab)[0].style.height = maxHeightOfTab+'px';
-
-					// >>> *****************************
-					// >>> handle content visibilty
-
-					var ankerTarget = '', $children,
-						nodeName    = $thisAElement.nodeName.toLowerCase();
-
-					if($lastShowedTab !== null){
-						$lastShowedTab.$addClass('hidden');
-					}
-
-					// > wenn <a>tag inhalt anzeigen
-					if(nodeName==='a'){
-						ankerTarget = $thisAElement.getAttribute('href').replace('#','');
-						$lastShowedTab = $($self.tabNamespaces+'[data-tab-content="'+ankerTarget+'"]')[0];
-						$lastShowedTab.$removeClass('hidden');
-						// > wenn <ul> dann innerhalb ul erstes li anzeigen wenn es nicht nested ist
-					} else {
-						$children = $thisAElement.children;
-						for(i= 0, length=$children.length; i<length;i++){
-							event.target=$children[i];
-							callback.call(null, event); break;
-						}
-					}
+            for(i=0, length=this.lastActiveTabs.length;i<length;i++){
+                this.lastActiveTabs[i].$addClass('taby-active');
+            }
 
 
+        },
+        setEvents : function(){
 
-				};
+            var event = {};
+                event.preventDefault=function(){};
 
-			// >>> first self call
-			event.target=$(this.tab + ' ul li')[0];
-			callback.call(null, event);
+            var $self = this,
+                $lastShowedTab=null,
+                callback = function(e){
 
-			// >>> handle tabs handler
-			$(this.tab + ' ul')[0].addEventListener('click', callback);
+                    e.preventDefault();
 
-			// >>> close tab handler
-			var $locks = $(this.tabLockEl);
+                    // >>> *****************************
+                    // >>> read operation
 
-			for(var i= 0, length=$locks.length; i<length;i++){
-				$locks[i].addEventListener('click', function(e){
-					if(!$lastShowedTab.$hasClass('hidden') && !$($self.tab)[0].$hasClass('hidden')){
-						$lastShowedTab.$addClass('hidden');
-						$($self.tab)[0].$addClass('hidden');
-					} else {
-						$($self.tab)[0].$removeClass('hidden');
-						event.target=$($self.tab + ' > ul > li')[0];
-						callback.call(null, event);
-					}
-				});
-			}
+                    // >>> start event delegation auslagern
+                    var target = e.target,
+                        $thisAElement = {},
+                        tmpUlContainer = [], i= 0, length=0;
 
-		}
+                    if(e.target.nodeName.toLowerCase() === 'a'){
+                        $thisAElement = e.target;
+                        target = e.target.parentNode;
+                    } else {
+                        $thisAElement = e.target.children[0];
+                        if(e.target.children.length===0){
+                            return;
+                        }
+                    }
 
-	};
+                    // >>> *******************************
+                    // >>> set active class
 
-	// Based off Lo-Dash's excellent UMD wrapper (slightly modified) - https://github.com/bestiejs/lodash/blob/master/lodash.js#L5515-L5543
-	// some AMD build optimizers, like r.js, check for specific condition patterns like the following:
-	if(typeof define === 'function' && define.amd) {
-		define(function(require) { return Taby; });
-	} else if(typeof module === 'object' && module.exports){
-		module.exports = Taby;
-	} else if(window.jQuery || window.Zepto){
-		(function ($) {
-			$.fn.taby = function () {
-				return new Taby(this);
-			};
-		})(window.jQuery || window.Zepto);
-	} else {
-		return Taby;
-	}
+                    $self.handleActiveTabs(target);
+
+                    // >>> ende delegation auslagern
+
+                    var $thisChildTarget = target.querySelector('ul'),
+                        $allTargets      = $(thisTaby.tab + ' ul');
+
+                    // > wenn kein ul, dann akuellen li auf
+                    //   $thisChildTarget setzen, damit
+                    //   aus dieser das parent ul ermittelt werden kann
+                    if($thisChildTarget===null) {
+                        $thisChildTarget = target;
+                    } else {
+                        tmpUlContainer.push($thisChildTarget);
+                    }
+
+                    // >>> start closest auslagern
+                    var parentClassName='';
+                    while(!/^taby$/.test(parentClassName)){
+                        var $parentNode = $thisChildTarget.parentNode;
+                        if($parentNode.nodeName.toLowerCase()==='ul'){
+                            tmpUlContainer.push($parentNode);
+                        }
+                        $thisChildTarget = $parentNode;
+                        parentClassName = $parentNode.className.replace(' ', '');
+
+                    }
+                    // >>> end closest auslagern
+
+                    // >>> *****************************
+                    // >>> write operation
+
+                    // > alle ul´s ausblenden
+                    for(i= 0, length=$allTargets.length; i<length;i++){
+                        $allTargets[i].$removeClass('show');
+                    }
+
+                    // > alle ul´s anzeigen die angezeigt werden sollen
+                    for(i=0, length=tmpUlContainer.length;i<length;i++){
+                        tmpUlContainer[i].$addClass('show');
+                    }
+
+                    // >>> *****************************
+                    // >>> set height of tab wrapper
+
+                    var maxHeightOfTab    = 0,
+                        $visibleListLayer = $($self.tab + ' ul.show > li:first-child');
+
+                    for(i=0, length=$visibleListLayer.length;i<length;i++){
+                        maxHeightOfTab += $visibleListLayer[i].clientHeight;
+
+                    }
+                    $($self.tab)[0].style.height = maxHeightOfTab+'px';
+
+                    // >>> *****************************
+                    // >>> handle content visibilty
+
+                    var ankerTarget = '', $children,
+                        nodeName    = $thisAElement.nodeName.toLowerCase();
+
+                    if($lastShowedTab !== null){
+                        $lastShowedTab.$addClass('hidden');
+                    }
+
+                    // > wenn <a>tag inhalt anzeigen
+                    if(nodeName==='a'){
+                        ankerTarget = $thisAElement.getAttribute('href').replace('#','');
+                        $lastShowedTab = $($self.tabNamespaces+'[data-tab-content="'+ankerTarget+'"]')[0];
+                        $lastShowedTab.$removeClass('hidden');
+                        // > wenn <ul> dann innerhalb ul erstes li anzeigen wenn es nicht nested ist
+                    } else {
+                        $children = $thisAElement.children;
+                        for(i= 0, length=$children.length; i<length;i++){
+                            event.target=$children[i];
+                            callback.call(null, event); break;
+                        }
+                    }
+
+
+
+                };
+
+            // >>> first self call
+            event.target=$(this.tab + ' ul li')[0];
+            callback.call(null, event);
+
+            // >>> handle tabs handler
+            $(this.tab + ' ul')[0].addEventListener('click', callback);
+
+            // >>> close tab handler
+            var $locks = $(this.tabLockEl);
+
+            for(var i= 0, length=$locks.length; i<length;i++){
+                $locks[i].addEventListener('click', function(e){
+                    if(!$lastShowedTab.$hasClass('hidden') && !$($self.tab)[0].$hasClass('hidden')){
+                        $lastShowedTab.$addClass('hidden');
+                        $($self.tab)[0].$addClass('hidden');
+                    } else {
+                        $($self.tab)[0].$removeClass('hidden');
+                        event.target=$($self.tab + ' > ul > li')[0];
+                        callback.call(null, event);
+                    }
+                });
+            }
+
+        }
+
+    };
+
+    // Based off Lo-Dash's excellent UMD wrapper (slightly modified) - https://github.com/bestiejs/lodash/blob/master/lodash.js#L5515-L5543
+    // some AMD build optimizers, like r.js, check for specific condition patterns like the following:
+    if(typeof define === 'function' && define.amd) {
+        define(function(require) { return Taby; });
+    } else if(typeof module === 'object' && module.exports){
+        module.exports = Taby;
+    } else if(window.jQuery || window.Zepto){
+        (function ($) {
+            $.fn.taby = function () {
+                return new Taby(this);
+            };
+        })(window.jQuery || window.Zepto);
+    } else {
+        return Taby;
+    }
 
 
 }(document, this, void(0)));
